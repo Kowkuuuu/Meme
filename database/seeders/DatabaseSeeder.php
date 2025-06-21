d7a8e961c43146541eac830cc0d6825feb80f6a9
Your Name <you@example.com>
2025-06-22 02:44:59 +0800

    commit
    # Conflicts:
    #	README.md

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@me.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
